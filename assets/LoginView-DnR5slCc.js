a39582924e6eb3495db0865dae389652997abb1c
Josue Cruz <jicruz@voiceteam.com>
2026-08-01 13:18:39 -0400

    Updates

---
+++ assets/LoginView-DnR5slCc.js
@@ -0,0 +1,1 @@
+import{H as f}from"./index-DqTsaLN4.js";export{f as default};
---
+++ assets/trash-2-a8wdytmh.js
@@ -0,0 +1,26 @@
+import{c as a}from"./index-DqTsaLN4.js";/**
+ * @license @lucide/vue v1.28.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M10 12h4",key:"a56b0p"}],["path",{d:"M10 8h4",key:"1sr2af"}],["path",{d:"M14 21v-3a2 2 0 0 0-4 0v3",key:"1rgiei"}],["path",{d:"M6 10H4a2 2 0 0 0-2 2v7a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-2",key:"secmi2"}],["path",{d:"M6 21V5a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v16",key:"16ra0t"}]],d=a("building-2",t);/**
+ * @license @lucide/vue v1.28.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],s=a("chevron-left",e);/**
+ * @license @lucide/vue v1.28.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],i=a("chevron-right",o);/**
+ * @license @lucide/vue v1.28.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=[["line",{x1:"12",x2:"12",y1:"2",y2:"22",key:"7eqyqh"}],["path",{d:"M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6",key:"1b0p4s"}]],y=a("dollar-sign",h);/**
+ * @license @lucide/vue v1.28.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["path",{d:"M10 11v6",key:"nco0om"}],["path",{d:"M14 11v6",key:"outv1u"}],["path",{d:"M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6",key:"miytrc"}],["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2",key:"e791ji"}]],p=a("trash-2",n);export{d as B,s as C,y as D,p as T,i as a};
